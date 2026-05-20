75d6b8aaa82fc43ff1275915fb8594fd805848a0
Paul GOMA <paulgoma07@gmail.com>
2026-05-21 00:29:28 +0200

    feat(auth): add customUser model with role field

--- src/miniledger_auth/users/models.py
+++ src/miniledger_auth/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+        Custom user model extending Django's AbstractUser.
+        
+        Adds a role field with predefined choices for access control.
+    """
+    class Role(models.TextChoices):
+        USER = "user", "User"
+        PREMIUM = "premium", "Premium"
+        ADMIN = "admin", "Admin"
+
+    role = models.CharField(
+        max_length=max(len(choice.value) for choice in Role),
+        choices=Role.choices,
+        default=Role.USER,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.role})"
